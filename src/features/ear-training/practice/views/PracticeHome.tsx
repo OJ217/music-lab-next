0fd54f4fde833ae01aea1bfb4581df4dad3db837
“Ochirjav <ochiroo032373@gmail.com>
2023-10-15 22:47:11 +0800

    Small modification

--- src/features/ear-training/practice/views/PracticeHome.tsx
+++ src/features/ear-training/practice/views/PracticeHome.tsx
@@ -11,7 +11,7 @@ const PracticeHome = () => {
 					href={'/ear-training/practice/interval'}
 					className='from block w-full rounded-xl border border-violet-600 bg-gradient-to-tr from-cyan-600/20 to-violet-600/25 px-8 py-6 transition-all duration-500 ease-in-out hover:opacity-80'
 				>
-					<h3 className='text-xl font-bold text-violet-300'>Interval</h3>
+					<h3 className='text-xl font-semibold text-violet-300'>Interval</h3>
 					<p className='text-sm text-gray-400'>Practice your Interval Identification skill</p>
 				</Link>
 
@@ -19,7 +19,7 @@ const PracticeHome = () => {
 					href={'/ear-training/practice/chord'}
 					className='from block w-full rounded-xl border border-violet-600 bg-gradient-to-tr from-cyan-600/20 to-violet-600/25 px-8 py-6 transition-all duration-500 ease-in-out hover:opacity-80'
 				>
-					<h3 className='text-xl font-bold text-violet-300'>Chord</h3>
+					<h3 className='text-xl font-semibold text-violet-300'>Chord</h3>
 					<p className='text-sm text-gray-400'>Practice your Chord Identification skill</p>
 				</Link>
 
@@ -27,7 +27,7 @@ const PracticeHome = () => {
 					href={'/ear-training/practice/mode'}
 					className='from block w-full rounded-xl border border-violet-600 bg-gradient-to-tr from-cyan-600/20 to-violet-600/25 px-8 py-6 transition-all duration-500 ease-in-out hover:opacity-80'
 				>
-					<h3 className='text-xl font-bold text-violet-300'>Mode</h3>
+					<h3 className='text-xl font-semibold text-violet-300'>Mode</h3>
 					<p className='text-sm text-gray-400'>Practice your Mode Identification skill</p>
 				</Link>
 			</div>
