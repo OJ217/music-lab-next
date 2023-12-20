ff01b5e218d1973d62b269f964d51ec4bb2e4954
“Ochirjav <ochiroo032373@gmail.com>
2023-12-21 02:14:29 +0800

    Apple status bar meta tag added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,10 @@ export default function App({ Component, pageProps }: AppProps) {
 							content='width=device-width, initial-scale=1, maximum-scale=1'
 						/>
 						<meta
+							name='apple-mobile-web-app-capable'
+							content='yes'
+						/>
+						<meta
 							name='apple-mobile-web-app-status-bar-style'
 							content='black-translucent'
 						/>
