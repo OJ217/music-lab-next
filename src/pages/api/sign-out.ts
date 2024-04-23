dd754fdeb2b28d690bfbd8ecaf1aa980cc9cde95
“Ochirjav <ochiroo032373@gmail.com>
2024-04-23 20:30:11 +0800

    ~Sign out api route set cookie header domain added

--- src/pages/api/sign-out.ts
+++ src/pages/api/sign-out.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
+	const domain = process.env.NODE_ENV === 'production' ? '.music-lab.app' : 'localhost';
 	res.setHeader('Set-Cookie', [
-		'music-lab.x-access-token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; Secure; SameSite=None; Partitioned',
-		'music-lab.x-refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; Secure; SameSite=None; Partitioned'
+		`music-lab.x-access-token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Domain=${domain}; HttpOnly; Secure; SameSite=None; Partitioned`,
+		`music-lab.x-refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Domain=${domain}; HttpOnly; Secure; SameSite=None; Partitioned`
 	]);
 	res.status(204).end();
 }
